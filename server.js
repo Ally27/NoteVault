76a36dd9a7bd050ee29dd584a9c4dfbd6dc40af7
Allyson Gonzales <Allysonmg21@gmail.com>
2023-03-17 00:10:26 -0700

    Created get and post route

--- server.js
+++ server.js
@@ -10,3 +10,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("./Develop/public"));
+// Get route
+app.get("/api/notes", function (req, res) {
+  readFileAsync("./Develop/db.json", "utf8").then(function (data) {
+    notes = [].concat(JSON.parse(userData));
+    res.json(notes);
+  });
+});
+// Post route
+app.post("./api/notes", function (req, res) {
+  const notes = req.body;
+  readFileAsync(".Develop/db/db.json", "utf8")
+    .then(function (userData) {
+      const note = [].concat(JSON.parse(userData));
+      notes.id = note.length + 1;
+      note.push(notes);
+      return notes;
+    })
+    .then(function (note) {
+      writeFileAsync("Develop/db/db.json", JSON.stringify(note));
+      res.json(notes);
+    });
+});
