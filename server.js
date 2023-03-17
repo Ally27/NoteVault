109290fce5e17be8101e8f5ad9bafa3b01014ba7
Allyson Gonzales <Allysonmg21@gmail.com>
2023-03-16 23:52:13 -0700

    set up server and middleware

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
-const util = require("util")
+const util = require("util");
 
-const PORT
+const PORT = 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("./Develop/public"));
